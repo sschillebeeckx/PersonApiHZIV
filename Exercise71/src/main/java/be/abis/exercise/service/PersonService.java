b103081c61506694c07639a09865f09d955b6ff1
Sandy Schillebeeckx <sschillebeeckx@abis.be>
2025-12-05 16:03:48 +0100

    database config

---
+++ Exercise71/src/main/java/be/abis/exercise/service/PersonService.java
@@ -0,0 +1,15 @@
+package be.abis.exercise.service;
+
+import be.abis.exercise.model.Person;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface PersonService {
+    Flux<Person> getAllPersons();
+    Mono<Person> findPerson(int id);
+    Mono<Person> findPerson(String emailAddress, String passWord);
+    Mono<Void> addPerson(Person p);
+    Mono<Void> deletePerson(int id);
+    Mono<Void> changePassword(Person p, String newPswd);
+    Flux<Person> findPersonsByCompanyName(String compName);
+}
